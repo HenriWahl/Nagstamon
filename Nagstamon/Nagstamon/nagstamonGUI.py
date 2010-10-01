88ddeca27af6bc46cd6a49d07516e9e939403ea0
Michał Rzeszut <mrzeszut@users.sourceforge.net>
2010-10-01 10:53:43 +0000

    another fix for rows-reordered event (explicitely invoking method at first click, and connecting every next click)

--- Nagstamon/Nagstamon/nagstamonGUI.py
+++ Nagstamon/Nagstamon/nagstamonGUI.py
@@ -163,8 +163,9 @@ class GUI(object):
         rows_reordered_handler = self.get_rows_reordered_handler(server)
         if rows_reordered_handler is not None:
             tab_model.disconnect(rows_reordered_handler)
+            new_rows_reordered_handler = tab_model.connect_after('rows-reordered', self.on_sorting_order_change, id, model, server)
+        else:
             self.on_sorting_order_change(tab_model, None, None, None, id, model, server)
-        new_rows_reordered_handler = tab_model.connect_after('rows-reordered', self.on_sorting_order_change, id, model, server)
         self.set_rows_reordered_handler(server, new_rows_reordered_handler)
         model.set_sort_column_id(id)
 
