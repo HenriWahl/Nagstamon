04747a94152096b4f9a4076a0e820a43899351d1
Henri Wahl <h.wahl@ifw-dresden.de>
2011-04-04 14:12:31 +0000

    Check_MK Multisite server type added

--- Nagstamon/Nagstamon/Custom.py
+++ Nagstamon/Nagstamon/Custom.py
@@ -9,6 +9,7 @@ from Nagstamon.Actions import register_server
 from Nagstamon.Server.Nagios import NagiosServer
 from Nagstamon.Server.Centreon import CentreonServer
 from Nagstamon.Server.Icinga import IcingaServer
+from Nagstamon.Server.Multisite import MultisiteServer
 from Nagstamon.Server.Ninja import NinjaServer
 from Nagstamon.Server.Opsview import OpsviewServer
 
@@ -17,6 +18,7 @@ from Nagstamon.Server.Opsview import OpsviewServer
 # order of registering affects sorting in server type list in add new server dialog
 register_server(NagiosServer)
 register_server(CentreonServer)
+register_server(MultisiteServer)
 register_server(IcingaServer)
 register_server(NinjaServer)
 register_server(OpsviewServer)
