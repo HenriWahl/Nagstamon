acb5439de89aa584e37752097c68a1bc2f820876
Henri Wahl <h.wahl@ifw-dresden.de>
2011-10-26 20:00:12 +0000

    fixed trouble with Icinga acknowledging services

--- Nagstamon/Nagstamon/Server/Icinga.py
+++ Nagstamon/Nagstamon/Server/Icinga.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 from Nagstamon.Server.Generic import GenericServer
+import urllib
 
 
 class IcingaServer(GenericServer):    
@@ -8,3 +9,43 @@ class IcingaServer(GenericServer):
         object of Incinga server
     """   
     TYPE = 'Icinga'
+
+    # Nagios CGI flags translation dictionary for acknowledging hosts/services 
+    HTML_ACKFLAGS = {True:"on", False:"off"}
+
+    
+    def _set_acknowledge(self, host, service, author, comment, sticky, notify, persistent, all_services=[]):
+        """
+        necessary port from 0.9.7.1 Generic.py because Icinga seems to have fixed Nagios bug
+        regarding treatment of ack flags - thus it does not work as it would with Nagios
+        """
+        url = self.nagios_cgi_url + "/cmd.cgi"      
+        
+        # decision about host or service - they have different URLs
+        # do not care about the doube %s (%s%s) - its ok, "flags" cares about the necessary "&"
+        if service == "":
+            # host
+            cgi_data = urllib.urlencode({"cmd_typ":"33", "cmd_mod":"2", "host":host, "com_author":author,\
+                                         "sticky_ack":self.HTML_ACKFLAGS[sticky], "send_notification":self.HTML_ACKFLAGS[notify], "persistent":self.HTML_ACKFLAGS[persistent],\
+                                         "com_data":comment, "btnSubmit":"Commit"})
+            self.FetchURL(url, giveback="raw", cgi_data=cgi_data) 
+            
+        # if host is acknowledged and all services should be to or if a service is acknowledged
+        # (and all other on this host too)
+        if service != "":
+            # service @ host
+            cgi_data = urllib.urlencode({"cmd_typ":"34", "cmd_mod":"2", "host":host, "service":service,\
+                                         "sticky_ack":self.HTML_ACKFLAGS[sticky], "send_notification":self.HTML_ACKFLAGS[notify], "persistent":self.HTML_ACKFLAGS[persistent],\
+                                         "com_author":author, "com_data":comment, "btnSubmit":"Commit"})          
+            # running remote cgi command        
+            self.FetchURL(url, giveback="raw", cgi_data=cgi_data) 
+
+        # acknowledge all services on a host
+        if len(all_services) > 0:
+            for s in all_services:
+                # service @ host
+                cgi_data = urllib.urlencode({"cmd_typ":"34", "cmd_mod":"2", "host":host, "service":s,\
+                                             "sticky_ack":self.HTML_ACKFLAGS[sticky], "send_notification":self.HTML_ACKFLAGS[notify], "persistent":self.HTML_ACKFLAGS[persistent],\
+                                             "com_author":author, "com_data":comment, "btnSubmit":"Commit"})
+                #running remote cgi command        
+                self.FetchURL(url, giveback="raw", cgi_data=cgi_data)
