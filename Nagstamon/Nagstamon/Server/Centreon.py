97b62630939a80eb94aad894dcc7b7bd09127e12
Henri Wahl <h.wahl@ifw-dresden.de>
2012-02-07 07:49:37 +0000

    fix in Centreon.py, changed shebang in nagstamon.py to use env (so it also runs on macos 10.5)

--- Nagstamon/Nagstamon/Server/Centreon.py
+++ Nagstamon/Nagstamon/Server/Centreon.py
@@ -256,7 +256,7 @@ class CentreonServer(GenericServer):
             self._get_ndo_url()
             
         # services (unknown, warning or critical?)
-        nagcgiurl_services = self.monitor_cgi_url + "/include/monitoring/status/Services" + self.XML_NDO + "/serviceXML.php?" + urllib.urlencode({"num":0, "limit":999, "o":"svcpb", "sort_type":"status", "sid":self.SID})
+        nagcgiurl_services = self.monitor_cgi_url + "/include/monitoring/status/Services/" + self.XML_NDO + "/serviceXML.php?" + urllib.urlencode({"num":0, "limit":999, "o":"svcpb", "sort_type":"status", "sid":self.SID})
 
         # hosts (up or down or unreachable)
         nagcgiurl_hosts = self.monitor_cgi_url + "/include/monitoring/status/Hosts/" + self.XML_NDO + "/hostXML.php?" + urllib.urlencode({"num":0, "limit":999, "o":"hpb", "sort_type":"status", "sid":self.SID})
