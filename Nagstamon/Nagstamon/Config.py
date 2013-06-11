9ed4ddc6fcc3028b30796acd7157e01d7da63cd6
Henri Wahl <h.wahl@ifw-dresden.de>
2013-06-11 13:45:05 +0200

    fixed Thruk merge

--- Nagstamon/Nagstamon/Config.py
+++ Nagstamon/Nagstamon/Config.py
@@ -295,8 +295,8 @@ class Config(object):
                     servers[server].password = ""
                 else:
                     servers[server].password = self.DeObfuscate(servers[server].password)
+                # do only deobfuscating if any autologin_key is set - will be only Centreon
                 if servers[server].__dict__.has_key("autologin_key"):
-                    print servers[server].__dict__
                     if len(servers[server].__dict__["autologin_key"]) > 0:
                         servers[server].autologin_key  = self.DeObfuscate(servers[server].autologin_key)
                 servers[server].proxy_username = self.DeObfuscate(servers[server].proxy_username)
