f2bf595846438579eeae489582eee334e2aa34ca
Henri Wahl <h.wahl@ifw-dresden.de>
2016-08-29 16:17:22 +0200

    prepare release 2.0-rc1

--- Nagstamon/Config.py
+++ Nagstamon/Config.py
@@ -57,7 +57,7 @@ class AppInfo(object):
         contains app information previously located in GUI.py
     """
     NAME = 'Nagstamon'
-    VERSION = '2.0-beta-20160829'
+    VERSION = '2.0-rc1'
     WEBSITE = 'https://nagstamon.ifw-dresden.de'
     COPYRIGHT = '©2008-2016 Henri Wahl et al.'
     COMMENTS = 'Nagios status monitor for your desktop'
