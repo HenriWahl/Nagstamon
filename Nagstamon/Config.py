5ac74ef3ffb6b306626657d778d76d862bc0dc90
Henri Wahl <h.wahl@ifw-dresden.de>
2018-11-29 11:20:54 +0100

    preparation of 3.2

--- Nagstamon/Config.py
+++ Nagstamon/Config.py
@@ -105,7 +105,7 @@ class AppInfo(object):
         contains app information previously located in GUI.py
     """
     NAME = 'Nagstamon'
-    VERSION = '3.1-20180829'
+    VERSION = '3.1.99'
     WEBSITE = 'https://nagstamon.ifw-dresden.de'
     COPYRIGHT = '©2008-2018 Henri Wahl et al.'
     COMMENTS = 'Nagios status monitor for your desktop'
