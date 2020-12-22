24d310612770ba72c18edb6399313d6ebb46664c
Henri Wahl <h.wahl@ifw-dresden.de>
2020-12-22 14:21:31 +0100

    try to fix https://github.com/HenriWahl/Nagstamon/issues/687

--- Nagstamon/Config.py
+++ Nagstamon/Config.py
@@ -124,7 +124,7 @@ class AppInfo(object):
         contains app information previously located in GUI.py
     """
     NAME = 'Nagstamon'
-    VERSION = '3.5-20201110'
+    VERSION = '3.5-20201222'
     WEBSITE = 'https://nagstamon.ifw-dresden.de'
     COPYRIGHT = '©2008-2020 Henri Wahl et al.'
     COMMENTS = 'Nagios status monitor for your desktop'
