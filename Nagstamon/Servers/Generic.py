fe3d7584194c74d393b8fda74a33ac52fb184f3b
dizixagi <dominik.zagol@ti-cahn.org>
2021-02-07 19:47:30 +0100

    fixing acknowleded services filter in zabbix

--- Nagstamon/Servers/Generic.py
+++ Nagstamon/Servers/Generic.py
@@ -1088,7 +1088,8 @@ class GenericServer(object):
                             self.Debug(server=self.get_name(),
                                        debug='Filter: SOFT STATE ' + str(host.name) + ';' + str(service.name))
                         service.visible = False
-                else:
+                # fix for https://github.com/HenriWahl/Nagstamon/issues/654
+                elif self.TYPE.startswith("Zabbix"):
                     if len(service.attempt) < 3:
                         service.visible = True
                     elif len(service.attempt) == 3:
