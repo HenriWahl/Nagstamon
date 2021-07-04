dae4d5f10aa769962aa682aaa2abc19e1a1d883c
Thomas Klausner <tk@giga.or.at>
2021-07-04 14:44:15 +0200

    Add NetBSD support to setup.py.

--- setup.py
+++ setup.py
@@ -39,6 +39,9 @@ if OS not in ['Windows', 'Darwin']:
     # platform.dist() returns "('', '', '')" on FreeBSD
     elif OS == 'FreeBSD':
         DIST, DIST_VERSION, DIST_NAME = ('', '', '')
+    # platform.dist() does not exist on NetBSD
+    elif OS == 'NetBSD':
+        DIST, DIST_VERSION, DIST_NAME = ('', '', '')
     else:
         DIST, DIST_VERSION, DIST_NAME = platform.dist()
     NAME = NAME.lower()
