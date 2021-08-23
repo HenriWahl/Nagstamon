587f6cd5f54ea0b74318cb5a411595531866b322
Henri Wahl <2835065+HenriWahl@users.noreply.github.com>
2021-08-23 21:05:01 +0200

    20210823

--- Nagstamon/Config.py
+++ Nagstamon/Config.py
@@ -125,7 +125,7 @@ class AppInfo(object):
         contains app information previously located in GUI.py
     """
     NAME = 'Nagstamon'
-    VERSION = '3.7-20210815'
+    VERSION = '3.7-20210823'
     WEBSITE = 'https://nagstamon.de'
     COPYRIGHT = '©2008-2021 Henri Wahl et al.'
     COMMENTS = 'Nagios status monitor for your desktop'
