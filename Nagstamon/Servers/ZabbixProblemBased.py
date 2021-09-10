6498ed992108244340ac9901cc69e9d1d43c2bb8
dizixagi <dominik.zagol@ti-cahn.org>
2021-09-10 23:08:40 +0200

    Add support for Zabbix 5.4.0+ and move from urllib to requests - Support for Zabbix 5.4.0+ (API changes, multiple interfaces support) - Persistent connections via requests (reduce load on Zabbix www server) - No longer using thirdparty.zabbix_api module - Code cleanup

--- Nagstamon/Servers/ZabbixProblemBased.py
+++ Nagstamon/Servers/ZabbixProblemBased.py
@@ -1,32 +1,104 @@
-# -*- encoding: utf-8; py-indent-offset: 4 -*-
-#
-# Zabbix.py based on Checkmk Multisite.py
+# encoding: utf-8
 
 import sys
-import urllib.parse
 import time
 import logging
-import datetime
+import requests
+from packaging.version import parse as parse_version
 
-from Nagstamon.Helpers import (HumanReadableDurationFromTimestamp,
-                               webbrowser_open)
+from Nagstamon.Helpers import HumanReadableDurationFromTimestamp
 from Nagstamon.Config import conf
-from Nagstamon.Objects import (GenericHost,
-                               GenericService,
-                               Result)
+from Nagstamon.Objects import GenericHost, GenericService, Result
 from Nagstamon.Servers.Generic import GenericServer
-from Nagstamon.thirdparty.zabbix_api import (ZabbixAPI,
-                                             ZabbixAPIException)
+
+class ZabbixLightApi():
+
+    logger = None
+    server_name = "Zabbix"
+    monitor_url = "http://127.0.0.1/api_jsonrpc.php"
+    validate_certs = False
+    r_session = None
+
+    zbx_auth = None
+    zbx_req_id = 0
+
+    def __init__(self, server_name, monitor_url, validate_certs):
+
+        self.server_name = server_name
+        self.monitor_url = monitor_url + "/api_jsonrpc.php"
+        self.validate_certs = validate_certs
+
+        #persistent connections
+        self.r_session = requests.Session()
+
+        #configure logging
+        self.logger = logging.getLogger('ZabbixLightApi_'+server_name)
+        console_logger = logging.StreamHandler()
+        console_logger_formatter = logging.Formatter('[%(name)s] %(levelname)s - %(message)s')
+        console_logger.setFormatter(console_logger_formatter)
+        self.logger.addHandler(console_logger)
+        if conf.debug_mode is True:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.INFO)
+        self.logger.debug("ZabbixLightApi START!")
+        self.logger.debug("monitor_url = " + self.monitor_url)
+
+    def do_request(self, method, params={}):
+        zabbix_rpc_obj = {
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params,
+            "auth": self.zbx_auth,
+            "id": self.zbx_req_id
+        }
+        self.zbx_req_id += 1
+
+        self.logger.debug("ZBX: > " + str(zabbix_rpc_obj))
+
+        try:
+
+            response = self.r_session.post(self.monitor_url, json=zabbix_rpc_obj, verify=self.validate_certs)
+
+            #we didnt get HTTP code 200
+            if response.status_code != 200:
+                raise ZabbixLightApiException("Got return code - " + str(response.status_code))
+
+            #parse response json
+            response_json = response.json()
+            self.logger.debug("ZBX: < " + str(response_json))
+
+            #there was some kind of error during processing our request
+            if "error" in response_json.keys():
+                raise ZabbixLightApiException("ZBX: < " + response_json["error"]["data"])
+
+            #zabbix returned garbage
+            if "result" not in response_json.keys():
+                raise ZabbixLightApiException("ZBX: < no result object in response")
+
+        #all other network related errors
+        except Exception as e:
+            raise ZabbixLightApiException(e)
+
+        return response_json['result']
+
+    def logged_in(self):
+        if self.zbx_auth is None:
+            return False
+        else:
+            return True
+
+    def login(self, username, password):
+        self.logger.debug("Login in as " + username)
+        self.zbx_auth = self.do_request('user.login', {'user': username, 'password': password})
+
+class ZabbixLightApiException(Exception):
+    pass
 
 class ZabbixProblemBasedServer(GenericServer):
 
     TYPE = 'ZabbixProblemBased'
-    zapi = None
-
-    if conf.debug_mode is True:
-        log_level = logging.DEBUG
-    else:
-        log_level = logging.WARNING
+    zlapi = None
 
     def __init__(self, **kwds):
         GenericServer.__init__(self, **kwds)
@@ -49,42 +121,34 @@ class ZabbixProblemBasedServer(GenericServer):
 
         self.username = conf.servers[self.get_name()].username
         self.password = conf.servers[self.get_name()].password
-        self.ignore_cert = conf.servers[self.get_name()].ignore_cert
-        self.validate_certs = not self.ignore_cert
-
-
-    def _login(self):
-        try:
-            # create ZabbixAPI if not yet created
-            if self.zapi is None:
-                self.zapi = ZabbixAPI(server=self.monitor_url, path="", log_level=self.log_level, validate_certs=self.validate_certs)
-            # login if not yet logged in, or if login was refused previously
-            if not self.zapi.logged_in():
-                self.zapi.login(self.username, self.password)
-        except ZabbixAPIException:
-            result, error = self.Error(sys.exc_info())
-            return Result(result=result, error=error)
+        self.validate_certs = not conf.servers[self.get_name()].ignore_cert
 
     def _get_status(self):
         """
             Get status from Zabbix Server
         """
-
-        # Login to ZabbixAPI
-        self._login()
-
         # =========================================
         # problems
         # =========================================
         problems = []
         try:
+            #Are we logged in?
+            if self.zlapi is None:
+                self.zlapi = ZabbixLightApi(server_name=self.name, monitor_url=self.monitor_url, validate_certs=self.validate_certs)
+                self.zbx_version = self.zlapi.do_request("apiinfo.version", {})
+            if not self.zlapi.logged_in():
+                self.zlapi.login(self.username, self.password)
+
             #Get all current problems (trigger based)
-            problems = self.zapi.problem.get({'recent': False})
+            if conf.filter_acknowledged_hosts_services:
+                problems = self.zlapi.do_request("problem.get", {'recent': False, 'acknowledged': False})
+            else:
+                problems = self.zlapi.do_request("problem.get", {'recent': False})
 
             for problem in problems:
 
                 #get trigger which rose current problem
-                trigger = self.zapi.trigger.get({'triggerids': problem['objectid'],
+                trigger = self.zlapi.do_request("trigger.get", {'triggerids': problem['objectid'],
                                                 'monitored': True,
                                                 'active': True,
                                                 'skipDependent': True,
@@ -114,29 +178,47 @@ class ZabbixProblemBasedServer(GenericServer):
                     if trigger[0]['hosts'][0]['maintenance_status'] == "1":
                         self.new_hosts[host_id].scheduled_downtime = True
 
-                    #host not available via agent
-                    if trigger[0]['hosts'][0]['available'] == "2":
-                        self.new_hosts[host_id].status = "DOWN"
-                        self.new_hosts[host_id].status_information = trigger[0]['hosts'][0]['error']
-                        self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(trigger[0]['hosts'][0]['errors_from'])
+                    #old api shows host interfaces status in host object
+                    if parse_version(self.zbx_version) < parse_version("5.4.0"):
 
-                    #host not available via ipmi
-                    if trigger[0]['hosts'][0].get('ipmi_available', '0') == "2":
-                        self.new_hosts[host_id].status = "DOWN"
-                        self.new_hosts[host_id].status_information = trigger[0]['hosts'][0]['ipmi_error']
-                        self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(trigger[0]['hosts'][0]['ipmi_errors_from'])
+                        #host not available via agent
+                        if trigger[0]['hosts'][0].get('available', '0') == "2":
+                            self.new_hosts[host_id].status = "DOWN"
+                            self.new_hosts[host_id].status_information = trigger[0]['hosts'][0]['error']
+                            self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(trigger[0]['hosts'][0]['errors_from'])
 
-                    #host not available via jmx
-                    if trigger[0]['hosts'][0].get('jmx_available', '0') == "2":
-                        self.new_hosts[host_id].status = "DOWN"
-                        self.new_hosts[host_id].status_information = trigger[0]['hosts'][0]['jmx_error']
-                        self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(trigger[0]['hosts'][0]['jmx_errors_from'])
+                        #host not available via ipmi
+                        if trigger[0]['hosts'][0].get('ipmi_available', '0') == "2":
+                            self.new_hosts[host_id].status = "DOWN"
+                            self.new_hosts[host_id].status_information = trigger[0]['hosts'][0]['ipmi_error']
+                            self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(trigger[0]['hosts'][0]['ipmi_errors_from'])
 
-                    #host not available via snmp
-                    if trigger[0]['hosts'][0].get('snmp_available', '0') == "2":
-                        self.new_hosts[host_id].status = "DOWN"
-                        self.new_hosts[host_id].status_information = trigger[0]['hosts'][0]['snmp_error']
-                        self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(trigger[0]['hosts'][0]['snmp_errors_from'])
+                        #host not available via jmx
+                        if trigger[0]['hosts'][0].get('jmx_available', '0') == "2":
+                            self.new_hosts[host_id].status = "DOWN"
+                            self.new_hosts[host_id].status_information = trigger[0]['hosts'][0]['jmx_error']
+                            self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(trigger[0]['hosts'][0]['jmx_errors_from'])
+
+                        #host not available via snmp
+                        if trigger[0]['hosts'][0].get('snmp_available', '0') == "2":
+                            self.new_hosts[host_id].status = "DOWN"
+                            self.new_hosts[host_id].status_information = trigger[0]['hosts'][0]['snmp_error']
+                            self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(trigger[0]['hosts'][0]['snmp_errors_from'])
+
+                    #new api shows host interfaces status in hostinterfaces object
+                    else:
+
+                        #get all host interfaces
+                        hostinterfaces = self.zlapi.do_request("hostinterface.get", {"hostids": host_id})
+
+                        #check them all and mark host as DOWN on first not available interface
+                        for hostinterface in hostinterfaces:
+                            if hostinterface.get('available', '0') == "2":
+                                self.new_hosts[host_id].status = "DOWN"
+                                self.new_hosts[host_id].status_information = hostinterface['error']
+                                self.new_hosts[host_id].duration = HumanReadableDurationFromTimestamp(hostinterface['errors_from'])
+                                #we stop checking rest of interfaces
+                                break
 
                 #service to report
                 self.new_hosts[host_id].services[service_id] = GenericService()
@@ -156,11 +238,10 @@ class ZabbixProblemBasedServer(GenericServer):
                 if problem['acknowledged'] == "1":
                     self.new_hosts[host_id].services[service_id].acknowledged = True
 
-        except (ZabbixAPIException):
+        except ZabbixLightApiException:
             # set checking flag back to False
             self.isChecking = False
             result, error = self.Error(sys.exc_info())
-            print(sys.exc_info())
             return Result(result=result, error=error)
 
         return Result()
