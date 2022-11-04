0a5696a8e0065bae9a582297d5a54ad5bc86365a
Henri <henri@nagstamon.de>
2022-11-05 00:47:43 +0100

    build debug old fstrings

--- build/build.py
+++ build/build.py
@@ -44,13 +44,14 @@ ARCH_OPTS = {'32': ('win32', 'win32', '', 'x86'),
 PYTHON_VERSION = '{0}.{1}'.format(sys.version_info[0],
                                   sys.version_info[1])
 
-# depending of debug build or not a console window will be shown or not
+# depending on debug build or not a console window will be shown or not
 if len(sys.argv) > 1 and sys.argv[1] == 'debug':
     GUI_MODE = '--console'
     FILE_SUFFIX = '_debug'
 else:
     GUI_MODE = '--windowed'
     FILE_SUFFIX = ''
+
 
 def winmain():
     """
@@ -74,9 +75,10 @@ def winmain():
 
     print('VERSION_IS:', VERSION_IS)
 
+    # old-school formatstrings needed for old Debian build base distro jessie and its old python
     ISCC = r'{0}{1}Inno Setup 6{1}iscc.exe'.format(os.environ['PROGRAMFILES{0}'.format(ARCH_OPTS[ARCH][2])], os.sep)
     DIR_BUILD_EXE = '{0}{1}dist{1}Nagstamon'.format(CURRENT_DIR, os.sep, ARCH_OPTS[ARCH][0], PYTHON_VERSION)
-    DIR_BUILD_NAGSTAMON = f'{CURRENT_DIR}{os.sep}dist{os.sep}Nagstamon-{VERSION}-win{ARCH}{FILE_SUFFIX}'
+    DIR_BUILD_NAGSTAMON = '{0}{1}dist{1}Nagstamon-{2}-win{3}{4}'.format(CURRENT_DIR, os.sep, VERSION, ARCH, FILE_SUFFIX)
     FILE_ZIP = '{0}.zip'.format(DIR_BUILD_NAGSTAMON)
 
     # clean older binaries
