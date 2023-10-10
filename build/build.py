4537339b142f4972e38d81f50b89d2929912b697
Henri Wahl <2835065+HenriWahl@users.noreply.github.com>
2023-10-10 22:18:06 +0200

    hidden imports in pyinstaller did not help

--- build/build.py
+++ build/build.py
@@ -114,8 +114,6 @@ def winmain():
                      '--icon=..\\Nagstamon\\resources\\nagstamon.ico',
                      '--name=Nagstamon',
                      '--hidden-import=PyQt6.uic.plugins',
-                     '--hidden-import=requests_kerberos',
-                     '--hidden-import=spnego',
                      '--hidden-import=win32timezone',
                      GUI_MODE,
                      '..\\nagstamon.py'],
