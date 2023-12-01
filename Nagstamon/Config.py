1003660f7fc6b09b75641d749c8d38feec40f3b8
Henri Wahl <2835065+HenriWahl@users.noreply.github.com>
2023-12-01 22:51:23 +0100

    QT_QPA_PLATFORMTHEME gnome

--- Nagstamon/Config.py
+++ Nagstamon/Config.py
@@ -68,6 +68,13 @@ if os.environ.get('CINNAMON_VERSION') or\
 else:
     DESKTOP_NEEDS_FIX = False
 
+# use QT platform plugins if not set otherwise on GNOME desktop
+# based on QGnomePlatform
+if not os.environ.get('QT_QPA_PLATFORMTHEME'):
+    if os.environ.get('XDG_SESSION_DESKTOP'):
+        if os.environ['XDG_SESSION_DESKTOP'].lower() == 'gnome':
+            os.environ['QT_QPA_PLATFORMTHEME'] = 'gnome'
+
 # queue.Queue() needs threading module which might be not such a good idea to be used
 # because QThread is already in use
 # maybe not the most logical place here to be defined but at least all
@@ -124,7 +131,7 @@ class AppInfo(object):
         contains app information previously located in GUI.py
     """
     NAME = 'Nagstamon'
-    VERSION = '3.13-20231130'
+    VERSION = '3.13-20231201'
     WEBSITE = 'https://nagstamon.de'
     COPYRIGHT = '©2008-2023 Henri Wahl et al.'
     COMMENTS = 'Nagios status monitor for your desktop'
@@ -754,14 +761,14 @@ class Config(object):
         """
         if OS == OS_WINDOWS:
             defaultactions = {"RDP": Action(name="RDP", description="Connect via RDP.",
-                                            type="command", string="C:\windows\system32\mstsc.exe /v:$ADDRESS$"),
+                                            type="command", string="C:/windows/system32/mstsc.exe /v:$ADDRESS$"),
                               "VNC": Action(name="VNC", description="Connect via VNC.",
-                                            type="command", string="C:\Program Files\TightVNC\vncviewer.exe $ADDRESS$"),
+                                            type="command", string="C:/Program Files/TightVNC/vncviewer.exe $ADDRESS$"),
                               "Telnet": Action(name="Telnet", description="Connect via Telnet.",
-                                               type="command", string="C:\Windows\System32\Telnet.exe root@$ADDRESS$"),
+                                               type="command", string="C:/Windows/System32\telnet.exe root@$ADDRESS$"),
                               "SSH": Action(name="SSH", description="Connect via SSH.",
                                             type="command",
-                                            string="C:\Program Files\PuTTY\putty.exe -l root $ADDRESS$")}
+                                            string="C:/Program Files/PuTTY/putty.exe -l root $ADDRESS$")}
         elif OS == OS_MACOS:
             defaultactions = {"RDP": Action(name="RDP", description="Connect via RDP.",
                                             type="command", string="open rdp://$ADDRESS$"),
