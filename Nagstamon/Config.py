c05c669318037699e13c5a96fbcba94bf83ab3cd
Henri Wahl <2835065+HenriWahl@users.noreply.github.com>
2024-01-08 22:27:51 +0100

    3.13-20240108

--- Nagstamon/Config.py
+++ Nagstamon/Config.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 # Nagstamon - Nagios status monitor for your desktop
-# Copyright (C) 2008-2023 Henri Wahl <henri@nagstamon.de> et al.
+# Copyright (C) 2008-2024 Henri Wahl <henri@nagstamon.de> et al.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -131,9 +131,9 @@ class AppInfo(object):
         contains app information previously located in GUI.py
     """
     NAME = 'Nagstamon'
-    VERSION = '3.13-20231208'
+    VERSION = '3.13-20240108'
     WEBSITE = 'https://nagstamon.de'
-    COPYRIGHT = '©2008-2023 Henri Wahl et al.'
+    COPYRIGHT = '©2008-2024 Henri Wahl et al.'
     COMMENTS = 'Nagios status monitor for your desktop'
     # dict of servers to offer for downloads if an update is available
     DOWNLOAD_SERVERS = {'nagstamon.de': 'https://github.com/HenriWahl/Nagstamon/releases'}
