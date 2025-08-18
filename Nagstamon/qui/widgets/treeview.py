23e4d248089f9f7f8d7fc33df24bb3ee2a4d719a
HenriWahl <henri@nagstamon.de>
2025-08-18 08:41:28 +0200

    margin again

--- Nagstamon/qui/widgets/treeview.py
+++ Nagstamon/qui/widgets/treeview.py
@@ -141,13 +141,14 @@ class TreeView(QTreeView):
         self.header().sortIndicatorChanged.connect(self.sort_columns)
 
         # set overall margin and hover colors - to be refined
-        self.setStyleSheet('''QTreeView::item {margin: 5px;}
+        self.setStyleSheet('''QTreeView::item {margin: 5px;
+                                               padding: 0x;}
                               QTreeView::item:hover {margin: 0px;
-                                                     padding: 5px;
+                                                     padding: 0px;
                                                      color: white;
                                                      background-color: dimgrey;}
                               QTreeView::item:selected {margin: 0px;
-                                                        padding: 5px;
+                                                        padding: 0px;
                                                         color: white;
                                                         background-color: grey;}
                             ''')
