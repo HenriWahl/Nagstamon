e72786fb01c28f3188bc83a30808fe4d92415227
Benoit Poulet <benoit.poulet@businessdecision.com>
2025-09-25 14:51:20 +0200

    improved errors handling on init

--- Nagstamon/servers/Centreon/__init__.py
+++ Nagstamon/servers/Centreon/__init__.py
@@ -36,22 +36,44 @@ class CentreonServer(GenericServer):
             # add it here to be able to fetch URL and ignore certs if activated
             self.ignore_cert = conf.servers[self.get_name()].ignore_cert
             self.custom_cert_use = conf.servers[self.get_name()].custom_cert_use
-            # This URL exists on Centreon 22.x - if not accessible it must be legacy
+            # This URL exists at least from Centreon 22.x - if not accessible it must be legacy
             versions_raw = self.fetch_url(f'{conf.servers[self.get_name()].monitor_cgi_url}/api/latest/platform/versions', no_auth=True, giveback='raw')
-            self.debug(server='[' + self.get_name() + ']', debug='Status code %s' % (str(versions_raw.status_code)))
+            self.debug(server='[' + self.get_name() + ']', debug='Page retrieval to detect the version, status code : %s' % (str(versions_raw.status_code)))
             if versions_raw.status_code == 200:
-                data = json.loads(versions_raw.result)
+                try:
+                    data = json.loads(versions_raw.result)
+                except Exception as e:
+                    self.debug(server='[' + self.get_name() + ']', debug='Page retrieval to detect the version, ERROR when decoding JSON')
+                    self.enable = False
+                    self.isChecking = False
+                    return None
+
                 ver_major = int(data["web"]["major"])
                 ver_minor = int(data["web"]["minor"])
                 # API V2 is usable only after 21.04 (not tested), ressources endpoint is buggy in 20.10
                 if ver_major >= 21:
                     self.debug(server='[' + self.get_name() + ']', debug='Loading class API, Centreon version : ' + str(ver_major) + '.' + str(ver_minor))
                     from .CentreonAPI import CentreonServer as CentreonServerReal
-                else:
+                elif ver_major < 21:
+                    # Fallback to Legacy
                     self.debug(server='[' + self.get_name() + ']', debug='Loading class LEGACY, Centreon version : ' + str(ver_major) + '.' + str(ver_minor))
                     from .CentreonLegacy import CentreonServer as CentreonServerReal
+                else:
+                    self.debug(server='[' + self.get_name() + ']', debug='Something wrong happens with Centreon, version detected : ' + str(ver_major) + '.' + str(ver_minor))
+                    self.enable = False
+                    self.isChecking = False
+                    return None
             else:
-                from .CentreonLegacy import CentreonServer as CentreonServerReal
-                self.debug(server='[' + self.get_name() + ']', debug='Loading class LEGACY, Centreon version will be checked later')
+                # Try to check if only it’s a Centreon
+                versions_raw = self.fetch_url(f'{server_conf.monitor_cgi_url}/main.php', no_auth=True, giveback='raw')
+                if versions_raw.status_code == 200:
+                    from .CentreonLegacy import CentreonServer as CentreonServerReal
+                    self.debug(server='[' + self.get_name() + ']', debug='Loading class LEGACY, Centreon version will be checked later')
+                else:
+                    self.debug(server='[' + self.get_name() + ']', debug='The URL given is not a Centreon')
+                    self.enable = False
+                    self.isChecking = False
+                    return None
+
             # kind of mad but helps the servers/__init__.py to detect if there is any other class to be used
             self.ClassServerReal = CentreonServerReal
