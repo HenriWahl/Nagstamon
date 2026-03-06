ea85f841b4d4c2e530dbab993f092903b750fd5d
Kimmig, Simon - D0242573 <simon.kimmig@dm.de>
2026-03-06 10:17:12 +0100

    prevent duplicate host creation in Zabbix.py

--- Nagstamon/servers/Zabbix.py
+++ Nagstamon/servers/Zabbix.py
@@ -280,11 +280,13 @@ class ZabbixServer(GenericServer):
                 if service['hosts']:
                     # Get the first host only, because we only support one host per service
                     for host in service['hosts']:
-                        self.new_hosts[host['name']] = GenericHost()
-                        self.new_hosts[host['name']].name = host['name']
-                        self.new_hosts[host['name']].server = self.name
-                        self.new_hosts[host['name']].status = 'UP'
-                        self.new_hosts[host['name']].scheduled_downtime = True if host["maintenance_status"] == '1' else False
+                        # Only create host if it does not exist yet
+                        if host['name'] not in self.new_hosts:
+                            self.new_hosts[host['name']] = GenericHost()
+                            self.new_hosts[host['name']].name = host['name']
+                            self.new_hosts[host['name']].server = self.name
+                            self.new_hosts[host['name']].status = 'UP'
+                            self.new_hosts[host['name']].scheduled_downtime = True if host["maintenance_status"] == '1' else False
                         # Map Stuff from Service to Host
                         self.new_hosts[host['name']].services[service["triggerid"]] = service_obj
                         self.new_hosts[host['name']].services[service["triggerid"]].host = host['name']
