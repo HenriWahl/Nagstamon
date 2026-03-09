74c2504e92026f6c8968c100aa2c8ed711da286a
HenriWahl <2835065+HenriWahl@users.noreply.github.com>
2026-03-09 21:53:25 +0100

    try truststore

--- Nagstamon/config.py
+++ Nagstamon/config.py
@@ -129,7 +129,7 @@ class AppInfo:
     contains app information previously located in GUI.py
     """
     NAME = 'Nagstamon'
-    VERSION = '3.18.1'
+    VERSION = '3.19-20260309'
     WEBSITE = 'https://nagstamon.de'
     COPYRIGHT = '©2008-2026 Henri Wahl et al.'
     COMMENTS = 'Nagios status monitor for your desktop'
