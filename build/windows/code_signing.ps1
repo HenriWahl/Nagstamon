a0ad6a81c330ba142354f00fa0f6b394930ba8b7
HenriWahl <2835065+HenriWahl@users.noreply.github.com>
2026-09-04 10:10:21 +0200

    codesigning again

--- build/windows/code_signing.ps1
+++ build/windows/code_signing.ps1
@@ -32,5 +32,4 @@ Write-Host "Signing file: $file"
 Write-Host "Using certificate thumbprint: ${env:CODESIGNING_THUMBPRINT}"
 
 # Sign the given file
-#& $signtool sign /debug /fd sha256 /sha1 ${env:CODESIGNING_THUMBPRINT} /tr http://ts.harica.gr /td sha256 $file
-C:\${env:ProgramFiles(x86)}\"Windows Kits"\10\bin\10.0.28000.0\x64\signtool.exe sign /debug /fd sha256 /sha1 ${env:CODESIGNING_THUMBPRINT} /tr http://ts.harica.gr /td sha256 $file
+& $signtool sign /debug /fd sha256 /sha1 ${env:CODESIGNING_THUMBPRINT} /tr http://ts.harica.gr /td sha256 $file
