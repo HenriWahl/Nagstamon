c775c863d147db00ea55298cae8d0b1f63b9881a
HenriWahl <2835065+HenriWahl@users.noreply.github.com>
2026-09-04 11:17:26 +0200

    codesigning again

--- build/windows/code_signing.ps1
+++ build/windows/code_signing.ps1
@@ -29,7 +29,6 @@ if (-not $signtool) {
 Write-Host "Current directory: $(Get-Location)"
 Write-Host "Using signtool: $signtool"
 Write-Host "Signing file: $file"
-Write-Host "Using certificate thumbprint: ${env:CODESIGNING_THUMBPRINT}"
 
 # Sign the given file
 & $signtool sign /debug /fd sha256 /sha1 ${env:CODESIGNING_THUMBPRINT} /tr http://ts.harica.gr /td sha256 $file
